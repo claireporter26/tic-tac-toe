f650f82e8b7aa744c80b4039f3923b4e186f8f52
Claire <clairelporter26@gmail.com>
2020-09-29 21:58:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let playerSymbol;
+let computerSymbol;
+
+const square = [];
+
+const square1 = document.getElementById("gameSquare1");
+const square2 = document.getElementById("gameSquare2");
+const square3 = document.getElementById("gameSquare3");
+const square4 = document.getElementById("gameSquare4");
+const square5 = document.getElementById("gameSquare5");
+const square6 = document.getElementById("gameSquare6");
+const square7 = document.getElementById("gameSquare7");
+const square8 = document.getElementById("gameSquare8");
+const square9 = document.getElementById("gameSquare9");
+
+const playerX = document.getElementById("chooseSideX");
+const playerO = document.getElementById("chooseSideO");
+
+const winningSymbol;
